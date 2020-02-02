5c5a7e020e6b6be3a8573397d9811726d881d31f
lucaisacoder <lucas_hsueh@hotmail.com>
2020-02-02 17:26:44 +0800

    core compile ok
    Change-Id: I3159b7908556fb3d86b5cc29c89caa3fbe6b9a18

--- code/src/core_api.c
+++ code/src/core_api.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "core.h"
+#include "log_api.h"
+#include "osal_api.h"
 
 #ifdef UNIT_TEST
 int core_api()
@@ -8,5 +10,7 @@ int main()
 #endif
 {
 	printf("CORE: core, FUNCTION:%s, VERSION:%d\n", __FUNCTION__, CORE_CORE_VERSION);
+	log_api();
+	osal_api();
 	return 0;
 }
